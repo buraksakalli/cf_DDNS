930d37b74b5bd28700560b10b35f16d74b1e7a3a
Burak Sakallı <buraksakalli06@gmail.com>
2020-07-03 20:13:12 +0300

    Requests added instead of cURL.

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # CloudFlare DDNS
 If you need a DDNS service you can use CloudFlare for free.
 
-# Using
-You have to change some informations in "comm" variable. You can find them in your CloudFlare Dashboard.
+## Usage
+You have to change some informations which are highlighted. You can find them in your CloudFlare Dashboard.
 
-It can work as CronJob. Add a command like;
-*/1 * * * * /usr/bin/python '/home/test/ddns.py'
+It can works as CronJob. Add a command like;
+
+`*/1 * * * * /usr/bin/python '/home/test/ddns.py'`
+
+## Finding DNS Record
+
+[Cloudflare Get Domain's DNS Record]('https://gist.github.com/buraksakalli/410ccb26cdb1ad168ede3bd6377128eb')
+
+```
+curl -X GET "https://api.cloudflare.com/client/v4/zones/ZONE_ID/dns_records?type=A&name=<Domain>" \
+     -H "X-Auth-Email: <email>" \
+     -H "X-Auth-Key: <Global API Key>" \
+     -H "Content-Type: application/json"
+```
--- ddns.py
+++ ddns.py
@@ -1,14 +1,25 @@
+import requests
 import json
-import urllib
-import os
 
-ip = urllib.urlopen('http://api.buraksakalli.org/ip/?f=json')
-data = json.loads(ip.read())
-j = data['ip']
+API_URL = 'https://api.buraksakalli.org/ip'
+API_RES = json.loads(requests.get(API_URL).content)['ip']
 
-comm = komut = 'curl -X PUT "https://api.cloudflare.com/client/v4/zones/YOUR_DDNS_ZONE_ID/dns_records/YOUR_DNS_RECORD" \\n     -H "X-Auth-Email: yourmailaddress@address.com" \\n     -H "X-Auth-Key: YOUR_AUTH_KEY" \\n     -H "Content-Type: application/json" \\n     --data \'{"type":"A","name":"YOUR_DDNS_NAME","content":"'
-cIp = j + "\"" + ',"ttl":120,"proxied":false}\''
+URL = 'https://api.cloudflare.com/client/v4/zones/<YOUR_DNS_ZONE>/dns_records/<YOUR_DNS_RECORD>'
 
-lastComm = comm+cIp
-os.system(lastComm)
-print lastComm
+headers = {
+  'X-Auth-Email': '<YOUR_EMAIL>',
+  'X-Auth-Key': '<YOUR_AUTH_KEY>',
+  'Content-Type': 'application/json'
+}
+
+data = {
+  "type":"A",
+  "name":"<DOMAIN_OR_SUBDOMAIN>", #to use DDNS
+  "content": API_RES,
+  "ttl": 120,
+  "proxied": "false"
+}
+
+CF_RES = requests.put(URL, headers=headers, data=json.dumps(data))
+
+print CF_RES.text
